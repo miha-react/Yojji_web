727594feedc3933f2aa82c9621e226ad20335184
miha-react <gosteev.mihail@gmail.com>
2018-03-14 12:24:35 +0200

    Add animation logic

--- src/utils/animate.js
+++ src/utils/animate.js
@@ -1,14 +1,22 @@
 import TimelineMax from './TimelineMax';
 import morphSVG from './MorphSVGPlugin';
+import TweenLite from './TweenLite';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const tl = new TimelineMax();
-
+  // const tl = new TimelineMax();
+  const svgCross = document.querySelector('.svg_cross');
   const cross = document.querySelector('.cross');
   const circle = document.querySelector('.circle_small');
 
-  cross.addEventListener('click', () => {
-    console.log('Test')
-    tl.to(cross, 1, { morphSVG: circle }, '0');
-  });
+  function transformCrossToCircle() {
+    if (svgCross.classList.contains('svg_cross') &&
+      !svgCross.classList.contains('transformToCircle')) {
+      TweenLite.to(cross, 0.5, { morphSVG: circle });
+      svgCross.classList.toggle('transformToCircle');
+    } else {
+      TweenLite.to(cross, 0.5, { morphSVG: cross });
+      svgCross.classList.toggle('transformToCircle');
+    }
+  }
+  svgCross.addEventListener('click', transformCrossToCircle);
 });
